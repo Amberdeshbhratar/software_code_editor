a5e58896f86d6f7276fa7b79b9ad22907c0ddec5
Amberdeshbhratar <93387201+Amberdeshbhratar@users.noreply.github.com>
2023-04-02 11:35:18 +0530

    socket change

--- server.js
+++ server.js
@@ -63,5 +63,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 'https://master--elaborate-bombolone-358524.netlify.app/';
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));

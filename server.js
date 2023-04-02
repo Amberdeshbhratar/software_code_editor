0e375fc9a72f239ca673d0024343508ea7810aa4
Amberdeshbhratar <93387201+Amberdeshbhratar@users.noreply.github.com>
2023-04-02 12:16:08 +0530

    env port backendurl

--- server.js
+++ server.js
@@ -63,5 +63,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 'https://master--elaborate-bombolone-358524.netlify.app/';
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
